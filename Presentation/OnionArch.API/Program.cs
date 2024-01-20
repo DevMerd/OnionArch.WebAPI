88caf67cfa26d8640102e37c5f4723d2f47b6da4
DevMerd <businessmert.kaya@gmail.com>
2024-01-20 13:27:01 +0300

    Dev ortamlarının ayrıştırılması ve appsetings yapılanmaları

--- Presentation/OnionArch.API/Program.cs
+++ Presentation/OnionArch.API/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var env = builder.Environment;
+builder.Configuration.SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
