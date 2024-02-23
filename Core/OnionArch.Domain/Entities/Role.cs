5ade4557c48d6f3bdc76c001ca7579df4a8a510f
DevMerd <businessmert.kaya@gmail.com>
2024-02-23 19:30:08 +0300

    Package restore and db context restore

---
+++ Core/OnionArch.Domain/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnionArch.Domain.Entities
+{
+    public class Role : IdentityRole<Guid>
+    {
+    }
+}
--- Infrastructure/OnionArch.Persistence/Context/AppDbContext.cs
+++ Infrastructure/OnionArch.Persistence/Context/AppDbContext.cs
@@ -6,7 +6,9 @@ namespace OnionArch.Persistence.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions options) : base(options)
         {
 
         }
